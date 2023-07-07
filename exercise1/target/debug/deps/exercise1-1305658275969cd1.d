960ed9d4003f809070cef21b47505b0ff1d3ecdf
Anthem <guoxiaoxiaoyu@gmail.com>
2023-07-07 22:46:23 +0200

    07.07

---
+++ exercise1/target/debug/deps/exercise1-1305658275969cd1.d
@@ -0,0 +1,7 @@
+/Users/anitya/Documents/GitHub/rust-exercise/exercise1/target/debug/deps/exercise1-1305658275969cd1.rmeta: src/main.rs src/module1/mod.rs src/module2/submodule/mod.rs
+
+/Users/anitya/Documents/GitHub/rust-exercise/exercise1/target/debug/deps/exercise1-1305658275969cd1.d: src/main.rs src/module1/mod.rs src/module2/submodule/mod.rs
+
+src/main.rs:
+src/module1/mod.rs:
+src/module2/submodule/mod.rs:
---
+++ exercise1/target/debug/deps/exercise1-de8d195e9b40e435.d
@@ -0,0 +1,7 @@
+/Users/anitya/Documents/GitHub/rust-exercise/exercise1/target/debug/deps/exercise1-de8d195e9b40e435.rmeta: src/main.rs src/module1/mod.rs src/module2/submodule/mod.rs
+
+/Users/anitya/Documents/GitHub/rust-exercise/exercise1/target/debug/deps/exercise1-de8d195e9b40e435.d: src/main.rs src/module1/mod.rs src/module2/submodule/mod.rs
+
+src/main.rs:
+src/module1/mod.rs:
+src/module2/submodule/mod.rs:
